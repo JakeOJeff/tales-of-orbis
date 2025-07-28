3a6626ef4241c238a36c3acc7639cc24005375fb
JakeOJeff <realjakeojeff@gmail.com>
2025-07-29 01:21:42 +0530

    Loading System

---
+++ scenes/loading.lua
@@ -0,0 +1,16 @@
+
+local loading = {}
+
+function loading:load()
+
+end
+
+function loading:update(dt)
+
+end
+
+function loading:draw()
+    love.graphics.print("LOADING...")
+end
+
+return loading
