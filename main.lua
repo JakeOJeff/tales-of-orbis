302bd0f8b21e7e1599d03b6ca56eb768b273a23e
JakeOJeff <realjakeojeff@gmail.com>
2025-07-28 23:24:13 +0530

    Declared source and hook for game and cutscene

--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@
 -- [INITIALIZING SCENERY]
 local SceneryInit = require("libs.scenery")
 local scenery = SceneryInit(
-    
+    {path = "scenes.loading", key = "loading", default = true},
+    {path = "scenes.cutscene", key = "cutscene"},
+    {path = "scenes.game", key = "game"}
 )
 scenery:hook(love)
 
---
+++ scenes/cutscene.lua
@@ -0,0 +1,16 @@
+
+local cutscene = {}
+
+function cutscene:load()
+
+end
+
+function cutscene:update(dt)
+
+end
+
+function cutscene:draw()
+
+end
+
+return cutscene
