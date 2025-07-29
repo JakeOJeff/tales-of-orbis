d3ac211bde0edd2ba60fadd895ce93a0a5cac61f
JakeOJeff <realjakeojeff@gmail.com>
2025-07-29 15:37:52 +0530

    Added loading images

--- src/loading.lua
+++ src/loading.lua
@@ -1,5 +1,12 @@
 
-local loading = {}
+local loading = {
+    imgs = {
+        love.graphics.newImage("assets/vfx/loading/light_load.png"),
+        love.graphics.newImage("assets/vfx/loading/night_load.png")
+    }
+}
+
+
 
 function loading:load()
 
@@ -10,7 +17,7 @@ function loading:update(dt)
 end
 
 function loading:draw()
-    love.graphics.print("LOADING...")
+    love.graphics.draw(loading.imgs[1], 0, 0)
 end
 
 return loading
