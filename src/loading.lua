b291186788b130f3ce83bf68ef3e9ca4c20ffd1a
JakeOJeff <realjakeojeff@gmail.com>
2025-07-29 15:49:13 +0530

    Loading shader

--- src/loading.lua
+++ src/loading.lua
@@ -14,14 +14,20 @@ function loading:load()
 end
 
 function loading:update(dt)
+        print("dt:", dt, "loaded:", loading.loaded)
 
+    loading.loaded = loading.loaded + (1 * dt)
+
+    if loading.loaded > 100 then
+        loading.setScene("game")
+    end
 end
 
 function loading:draw()
     love.graphics.draw(loading.imgs[1], 0, 0)
 
     love.graphics.setScissor(0, 0, wW, loading.loaded * wH)
-    love.graphics.draw(loading.imgs[1], 0, 0)
+    love.graphics.draw(loading.imgs[2], 0, 0)
     love.graphics.setScissor()
 end
 
