5a3065842ac850de6a9f98ffa42b7d9e47eed5e6
JakeOJeff <realjakeojeff@gmail.com>
2025-07-29 16:44:46 +0530

    Implemented a working loading text

--- src/loading.lua
+++ src/loading.lua
@@ -2,6 +2,7 @@ local loading = {
     imgs = {love.graphics.newImage("assets/vfx/loading/light_load.png"),
             love.graphics.newImage("assets/vfx/loading/night_load.png")},
     loaded = 0, -- In percentages
+    time = 0,
     text = "loading."
 }
 
@@ -10,26 +11,36 @@ function loading:load()
 end
 
 function loading:update(dt)
-    print("dt:", dt, "loaded:", loading.loaded)
+    print("dt:", dt, "loaded:", self.loaded)
 
-    loading.loaded = loading.loaded + (5 * dt)
+    self.loaded = self.loaded + (5 * dt)
+    self.time = self.time + (1 * dt)
 
-    if loading.loaded > 120 then
-        loading.setScene("game")
-    elseif loading.loaded > 100 then
-        loading.text = "loaded."
+    if (self.text ~= "loading....")then
+        if self.time > 2 then
+            self.text = self.text .. "."
+            self.time = 0
+        end
+    else
+        self.text = "loading."
+    end
+
+    if self.loaded > 125 then
+        self.setScene("game")
+    elseif self.loaded > 100 then
+        self.text = "loaded."
     end
 end
 
 function loading:draw()
-    love.graphics.draw(loading.imgs[1], 0, 0)
+    love.graphics.draw(self.imgs[1], 0, 0)
 
-    love.graphics.setScissor(0, 0, wW, loading.loaded / 100 * wH)
-    love.graphics.draw(loading.imgs[2], 0, 0)
+    love.graphics.setScissor(0, 0, wW, self.loaded / 100 * wH)
+    love.graphics.draw(self.imgs[2], 0, 0)
     love.graphics.setScissor()
 
     love.graphics.setFont(heading)
-    love.graphics.print(loading.text, wW/2 - heading:getWidth(loading.text)/2, wH/2 - heading:getHeight()/2)
+    love.graphics.print(self.text, wW/2 - heading:getWidth(self.text)/2, wH/2 - heading:getHeight()/2)
 end
 
 return loading
