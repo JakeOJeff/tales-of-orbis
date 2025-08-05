18200d720408c16ce4e948f7b781e687dff9c656
JakeOJeff <realjakeojeff@gmail.com>
2025-08-05 13:37:31 +0530

    Drawing playbutton along with the final image

--- src/loadScenes/title.lua
+++ src/loadScenes/title.lua
@@ -4,10 +4,11 @@ local title = {
         love.graphics.newImage("assets/vfx/loading/titles/tales-of-text.png"),
         love.graphics.newImage("assets/vfx/loading/titles/orb-light.png"),
         love.graphics.newImage("assets/vfx/loading/titles/orb-text.png"),
+        love.graphics.newImage("assets/vfx/loading/titles/play.png") -- Play button is last
     },
     timer = 0,
-    fadeTime = 1, -- seconds for fade in
-    delayBetween = 3,
+    fadeTime = 1,         -- seconds to fade each image
+    delayBetween = 3,     -- delay between each image
     startTime = nil,
     play_x = 527,
     play_y = 340,
@@ -15,29 +16,34 @@ local title = {
     play_height = 90,
     normal_play = love.graphics.newImage("assets/vfx/loading/titles/play.png"),
     hover_play = love.graphics.newImage("assets/vfx/loading/titles/play-hover.png"),
-    currentPlayButton = nil,
 }
+
+
 
 function title:load()
     self.startTime = love.timer.getTime()
-    self.currentPlayButton = self.normal_play
 end
 
 function title:update(dt)
     self.timer = love.timer.getTime() - self.startTime
-    local mx, my = normalizeCoords(love.mouse.getPosition())
-    self:handlePlayHover(mx, my, love.mouse.isDown(1))
-end
 
-function title:handlePlayHover(x, y, isPressed)
-    local inPlay = distRect(x, y, self.play_x, self.play_y, self.play_width, self.play_height)
-    self.currentPlayButton = inPlay and self.hover_play or self.normal_play
-    if inPlay and isPressed then
+    local mx, my = normalizeCoords(love.mouse.getPosition())
+    local inPlay = distRect(mx, my, self.play_x, self.play_y, self.play_width, self.play_height)
+
+    -- Swap play button image if hovered
+    self.imgs[5] = inPlay and self.hover_play or self.normal_play
+
+    -- Handle click
+    if inPlay and love.mouse.isDown(1) then
         title.setScene("intro")
     end
 end
 
 function title:draw()
+    love.graphics.push()
+    love.graphics.scale(scale, scale)
+    love.graphics.translate(cenW, cenH)
+
     for i, img in ipairs(self.imgs) do
         local appearTime = (i - 1) * self.delayBetween
         local t = self.timer - appearTime
@@ -45,21 +51,12 @@ function title:draw()
         if t >= 0 then
             local alpha = math.min(t / self.fadeTime, 1)
             love.graphics.setColor(1, 1, 1, alpha)
-            love.graphics.push()
-            love.graphics.scale(scale, scale)
-            love.graphics.translate(cenW, cenH)
             love.graphics.draw(img, 0, 0)
-            love.graphics.pop()
         end
     end
 
-    -- Draw play button last
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.push()
-    love.graphics.scale(scale, scale)
-    love.graphics.translate(cenW, cenH)
-    love.graphics.draw(self.currentPlayButton, self.play_x, self.play_y)
     love.graphics.pop()
+    love.graphics.setColor(1, 1, 1, 1)
 end
 
 function title:keypressed(key)
@@ -77,8 +74,13 @@ end
 
 function title:touchpressed(id, x, y)
     isMobile = true
-    local normX, normY = normalizeCoords(x, y)
-    self:handlePlayHover(normX, normY, true)
+    local tx, ty = normalizeCoords(x, y)
+    local inPlay = distRect(tx, ty, self.play_x, self.play_y, self.play_width, self.play_height)
+    self.imgs[5] = inPlay and self.hover_play or self.normal_play
+
+    if inPlay then
+        title.setScene("intro")
+    end
 end
 
 return title
