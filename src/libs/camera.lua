fc87a697792f953656ba9d0aadd259b0b201a8fe
JakeOJeff <realjakeojeff@gmail.com>
2025-08-06 00:40:57 +0530

    Clamped Camera for y and x

--- src/libs/camera.lua
+++ src/libs/camera.lua
@@ -18,21 +18,32 @@ end
 function Camera:clear()
     love.graphics.pop()
 end
-
 function Camera:setPosition(x, y)
+    local screenW = love.graphics.getWidth()
+    local screenH = love.graphics.getHeight()
 
-    self.x = x - love.graphics.getWidth() / 2 / self.scale
-    self.y = y - love.graphics.getHeight() / 2 / self.scale
+    -- Set centered position
+    self.x = x - screenW / 2 / self.scale
+    self.y = y - screenH / 2 / self.scale
 
-    local rH = self.x + love.graphics.getWidth() /2
+    -- Right and bottom bounds
+    local rightEdge = self.x + screenW / self.scale
+    local bottomEdge = self.y + screenH / self.scale
 
+    -- Clamp X
     if self.x < 0 then
         self.x = 0
-    elseif rH > MapWidth then 
-        self.x = MapWidth - love.graphics.getWidth() / 2
+    elseif rightEdge > MapWidth then 
+        self.x = MapWidth - screenW / self.scale
     end
 
-
+    -- Clamp Y
+    if self.y < 0 then
+        self.y = 0
+    elseif bottomEdge > MapHeight then
+        self.y = MapHeight - screenH / self.scale
+    end
 end
+
 
 return Camera
