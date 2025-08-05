2cebc02cb361f580f28896ed871e312286d0be5c
JakeOJeff <realjakeojeff@gmail.com>
2025-08-05 19:42:05 +0530

    Configuration updated for android

--- conf.lua
+++ conf.lua
@@ -1,6 +1,8 @@
 function love.conf(t)
     t.window.width = 1280
     t.window.height = 720
+    t.window.fullscreen = love.system.getOS() == "Android"
+    t.window.fullscreentype = "exclusive" -- Or "desktop" if you want it windowed fullscreen
     t.console = false
     -- t.window.resizable = true
     t.window.usedpiscale = false
