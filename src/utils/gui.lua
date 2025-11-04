46dd200748fdeb5bef1eec561586222d7ccccb07
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-04 20:41:31 +0530

    Fixing scaling issues

--- src/utils/gui.lua
+++ src/utils/gui.lua
@@ -2,7 +2,7 @@ GUI = {}
 
 function GUI:load()
     self.buttons = {}
-    self.buttons.leftButton = self:createButton("assets/vfx/icons/left.png", 20, wH - 20 )
+    self.buttons.leftButton = self:createButton("assets/vfx/icons/left.png", 20, wH - 80 )
     self.buttons.rightButton = self:createButton("assets/vfx/icons/right.png", 20 + 10 + self.buttons.leftButton.src:getWidth(), wH - 20 )
     self.buttons.boostButton = self:createButton("assets/vfx/icons/boost.png", 50, 50)
     self.buttons.jumpButton = self:createButton("assets/vfx/icons/jump.png", 100, 100)
@@ -30,7 +30,7 @@ end
 function GUI:draw()
     if IsMobile and not paused then
         local b = self.buttons
-        self:drawButton(b.leftButton, 1.5, 20)
+        self:drawButton(b.leftButton,20)
     end
 end
 
@@ -38,8 +38,8 @@ function GUI:createButton(src, x, y, w, h, cond)
     local table = {}
     table.x = x * scale
     table.y = y * scale
-    table.w = w * scale or 50 * scale
-    table.h = h * scale or 50 * scale
+    table.w = (w  or 50 ) * scale
+    table.h = (h  or 50) * scale
     table.src = LG.newImage(src)
     table.cond = cond or function ()
         return true
@@ -54,10 +54,9 @@ function GUI:drawButton(v, round)
     if v.holding then
         LG.setColor(0.1, 0.1, 0.1, 0.6)
     end
-    local w, h = v.src:getWidth() * scale, v.src:getHeight()  * scale
-    LG.rectangle("fill", v.x, v.y, w, h, round, round)
+    LG.rectangle("fill", v.x, v.y, v.w, v.h, round, round)
     LG.setColor(1, 1, 1)
-    LG.draw(v.src, v.x + w/2 + (v.src:getWidth()/2), v.y + h/2 + (v.src:getHeight()/2 ), 0, scale, scale)
+    self:drawButtonImage(v)
     LG.setColor(0, 0, 0, 0.6)
 end
 function GUI:drawButtonImage(button)
