2967f3e8e041d2130bdd8cdcdd4a4abf13e294e8
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-06 02:50:04 +0530

    Updated flsl

--- src/shaders/darkness.glsl
+++ src/shaders/darkness.glsl
@@ -1,26 +1,26 @@
-        // extern vec2 lightPos;
-        // extern float lightRadius;
-        // extern float ambient;
+extern vec2 lightPos;
+extern float lightRadius;
+extern float ambient;
 
-        // vec4 effect(vec4 color, Image tex, vec2 texCoord, vec2 screenCoord) {
-        //     float dist = distance(screenCoord, lightPos);
-        //     float intensity = clamp(1.0 - dist / lightRadius, 0.0, 1.0);
-        //     float brightness = clamp(ambient + intensity, 0.0, 1.0);
-        //     vec4 texColor = Texel(tex, texCoord);
-        //     return texColor * vec4(vec3(brightness), 1.0);
-        // }
+vec4 effect(vec4 color, Image tex, vec2 texCoord, vec2 screenCoord) {
+    float dist = distance(screenCoord, lightPos);
+    float intensity = clamp(1.0 - dist / lightRadius, 0.0, 1.0);
+    float brightness = clamp(ambient + intensity, 0.0, 1.0);
+    vec4 texColor = Texel(tex, texCoord);
+    return texColor * vec4(vec3(brightness), 1.0);
+}
 
-    uniform vec2 lightPos;    
-    uniform float lightRadius;  
+    // uniform vec2 lightPos;    
+    // uniform float lightRadius;  
 
-    vec4 effect(vec4 color, Image texture, vec2 texCoords, vec2 screenCoords) {
-        float dist = distance(screenCoords, lightPos);
-        vec4 texColor = texture2D(texture, texCoords);
-        if (dist < lightRadius) {
-            return vec4(texColor.rgb, 1);
-        }
-        else {
-            return vec4(0, 0, 0, 1);
-        }
+    // vec4 effect(vec4 color, Image texture, vec2 texCoords, vec2 screenCoords) {
+    //     float dist = distance(screenCoords, lightPos);
+    //     vec4 texColor = texture2D(texture, texCoords);
+    //     if (dist < lightRadius) {
+    //         return vec4(texColor.rgb, 1);
+    //     }
+    //     else {
+    //         return vec4(0, 0, 0, 1);
+    //     }
 
-    }
+    // }
