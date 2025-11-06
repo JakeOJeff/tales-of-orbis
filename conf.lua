c854c8e3a5ec059fbd8b4aa4155e64fb637c43ea
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-06 22:57:57 +0530

    Fixing orientation issues

--- conf.lua
+++ conf.lua
@@ -3,8 +3,8 @@ function love.conf(t)
     t.window.height = 720  -- 1080 
     t.window.title = "Tales of Orbis"
     t.window.fullscreen = false
-    t.console = true
-    t.window.resizable = true
+    t.console = false
+    t.window.resizable = false
     t.window.usedpiscale = false
     t.externalstorage = true
 end
