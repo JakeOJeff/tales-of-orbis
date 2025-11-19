071d87e0dfbeac5bc5fbb6922bd639fa262ae5b8
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-19 19:52:10 +0530

    Loading al lthe images

---
+++ src/classes/textbox.lua
@@ -0,0 +1,26 @@
+Textbox = {}
+Textbox.__index = Textbox
+Textboxes = {}
+
+local imageset = {
+    le = love.graphics.newImage("assets/vfx/textbox/ledge.png"),
+    re = love.graphics.newImage("assets/vfx/textbox/redge.png"),
+    te = love.graphics.newImage("assets/vfx/textbox/tedge.png"),
+    be = love.graphics.newImage("assets/vfx/textbox/bedge.png"),
+    tlc = love.graphics.newImage("assets/vfx/textbox/tlcorner.png"),
+    trc = love.graphics.newImage("assets/vfx/textbox/trcorner.png"),
+    blc = love.graphics.newImage("assets/vfx/textbox/blcorner.png"),
+    brc = love.graphicsa.newImage("assets/vfx/textbox/brcorner.png"),
+    cen = love.graphics.newImage("assets/vfx/textbox/center.png")
+}
+
+function Textbox:new(x, y, width, height)
+    local instance = setmetatable({}, Textbox)
+
+    instance.x = x
+    instance.y = y
+    instance.width = width
+    instance.height = height
+
+    
+end
