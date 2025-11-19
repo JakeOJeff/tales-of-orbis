36e9fe697b786c00c76706ed1dd23fb4799e7bdd
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-19 17:52:54 +0530

    Fixed lighting

--- src/shaders/darkness.glsl
+++ src/shaders/darkness.glsl
@@ -22,7 +22,7 @@ vec4 effect(vec4 color, Image texture, vec2 texCoords, vec2 screenCoords) {
     float finalBrightness = max(mainBrightness, ambient);
     
     // Apply light color to the brightness
-    vec3 tintedLight = vec3(1.0, 0.5, 0.2) * finalBrightness;
+    vec3 tintedLight = vec3(1.0, 0.870, 0.403) * finalBrightness;
     
     // Multiply texture color with the tinted light
     vec3 litColor = texColor.rgb * tintedLight;
