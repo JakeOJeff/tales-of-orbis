33675497847037a58c5e10d843f9ba3f8ac95922
Paulyn <jakeojeff@gmail.com>
2026-01-26 02:46:21 +0530

    Working added end systems

--- src/loadScenes/end.lua
+++ src/loadScenes/end.lua
@@ -8,7 +8,6 @@ local endScene = {
         "I might refactor it again ehehe",
         "I don't really know what to do",
         "Wait for the next part, I guess :D"
-    
     }
 }
 
@@ -16,7 +15,7 @@ local endScene = {
 function endScene:load()
     self.index = 1
     self.time = 0
-    self.timeInterval = 5
+    self.timeInterval = 2
 
     self.pos = {
         x = 0,
@@ -30,16 +29,22 @@ function endScene:update(dt)
 
     if self.time > self.timeInterval then
         self.time = 0
+        if self.index < #self.text then
+            self.index = self.index + 1
+        end
     end
-    self.pos.x = subheading:getWidth(self.text[self.index])
+    self.pos.x = wW/2 - paragraph:getWidth(self.text[self.index])/2
 
-    self.pos.y = (wH / 2 + subheading:getHeight()/2 + 40) - 40 * self.time/self.timeInterval
+    self.pos.y = (wH / 2 + paragraph:getHeight()/2 + 40) - 40 * self.time/self.timeInterval
 end
 
 function endScene:draw()
-    lg.setFont(subheading)
-    lg.setColor(1,1,1,self.time/self.timeInterval)
-    lg.print(self.text[self.index], self.pos.x, self.pos.y)
+    love.graphics.setFont(paragraph)
+    love.graphics.setColor(1,1,1,self.time/self.timeInterval)
+    love.graphics.print(self.text[self.index], self.pos.x, self.pos.y)
 end
 
 return endScene
+
+
+
